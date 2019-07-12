ad85a9891a15f5049bfe4ab3285bdcb95e6b1ad5
Olayioye Ibrahim Oluwafemi <ibraphem@gmail.com>
2019-07-12 08:14:43 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,69 @@
+<div class="post-search-panel">
+    <input type="text" id="keywords" placeholder="Type keywords to filter posts" onkeyup="searchFilter()"/>
+    <select id="sortBy" onchange="searchFilter()">
+        <option value="">Sort By</option>
+        <option value="asc">Ascending</option>
+        <option value="desc">Descending</option>
+    </select>
+</div>
+<div class="post-wrapper">
+    <div class="loading-overlay"><div class="overlay-content">Loading.....</div></div>
+    <div id="posts_content">
+    <?php
+    //Include pagination class file
+    include('Pagination.php');
+    
+    //Include database configuration file
+    include('dbConfig.php');
+    
+    $limit = 10;
+    
+    //get number of rows
+    $queryNum = $db->query("SELECT COUNT(*) as postNum FROM posts");
+    $resultNum = $queryNum->fetch_assoc();
+    $rowCount = $resultNum['postNum'];
+    
+    //initialize pagination class
+    $pagConfig = array(
+        'totalRows' => $rowCount,
+        'perPage' => $limit,
+        'link_func' => 'searchFilter'
+    );
+    $pagination =  new Pagination($pagConfig);
+    
+    //get rows
+    $query = $db->query("SELECT * FROM posts ORDER BY id DESC LIMIT $limit");
+    
+    if($query->num_rows > 0){ ?>
+        <div class="posts_list">
+        <?php
+            while($row = $query->fetch_assoc()){ 
+                $postID = $row['id'];
+        ?>
+            <div class="list_item"><a href="javascript:void(0);"><h2><?php echo $row["title"]; ?></h2></a></div>
+        <?php } ?>
+        </div>
+        <?php echo $pagination->createLinks(); ?>
+    <?php } ?>
+    </div>
+</div>
+
+<script>
+function searchFilter(page_num) {
+    page_num = page_num?page_num:0;
+    var keywords = $('#keywords').val();
+    var sortBy = $('#sortBy').val();
+    $.ajax({
+        type: 'POST',
+        url: 'getData.php',
+        data:'page='+page_num+'&keywords='+keywords+'&sortBy='+sortBy,
+        beforeSend: function () {
+            $('.loading-overlay').show();
+        },
+        success: function (html) {
+            $('#posts_content').html(html);
+            $('.loading-overlay').fadeOut("slow");
+        }
+    });
+}
+</script>
